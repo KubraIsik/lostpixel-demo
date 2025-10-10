03357c08eecaa0c35aac2d5c28ca922cd0921cc6
KubraIsik <kuisik@gmail.com>
2025-10-10 18:36:26 +0300

    Add Lost Pixel configuration and Storybook setup

---
+++ lostpixel.config.ts
@@ -0,0 +1,11 @@
+// lostpixel.config.ts
+import type { CustomProjectConfig } from 'lost-pixel';
+
+const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: 'http://localhost:6006',
+  },
+  generateOnly: true,
+};
+
+export default config;
